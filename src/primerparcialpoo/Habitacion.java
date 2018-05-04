38ea952d76d6d4e438344a7f2f815c171b3af363
Steven Diaz <>
2018-05-04 02:25:00 -0600

    prototipo ag pisos

--- src/primerparcialpoo/Habitacion.java
+++ src/primerparcialpoo/Habitacion.java
@@ -21,6 +21,7 @@ public class Habitacion {
     private int PrecioDeHabitacion;
     ArrayList<String> Piso = new ArrayList<>();
     ArrayList<String> NumeroHabitacion = new ArrayList<>();
+    String [] PisosExtras = {"G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 
     public Habitacion(){}
     
@@ -42,9 +43,66 @@ public class Habitacion {
         Piso.add("E");
         Piso.add("F");
     }
+    public void agregarPiso(){
+        System.out.println("Ingrese las cantidades de Pisos a agregar:");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        for (int i = 0; i < a; i++){
+         
+             
+        }
+    }
     
+    public void recibirNuevoPiso() {
+        System.out.println("Ingrese las cantidades de Pisos a agregar:");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        switch (a){
+            case 1:
+                Piso.add("H");
+                break;
+            case 2:
+                Piso.add("H");
+                Piso.add("I");
+                break;
+            case 3:
+                Piso.add("H");
+                Piso.add("I");
+                Piso.add("J");
+                break;
+            case 4:
+                Piso.add("H");
+                Piso.add("I");
+                Piso.add("J");
+                Piso.add("J");
+                break;
+            case 5:
+                Piso.add("H");
+                Piso.add("I");
+                Piso.add("J");
+                Piso.add("K");
+                Piso.add("L");
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            default:
+                System.out.println("Por favor ingrese una cantidad valida");
+                recibirNuevoPiso();
+                break;
+        }
+    }
     public void recibirNuevaHabitacion() {
-        
+        System.out.println("Ingrese las cantidades de habitacines a agregar:");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
     }
     
     public void MostrarPisos(){
@@ -74,7 +132,7 @@ public class Habitacion {
     
     
     public void Disponibilidad(){
-        boolean reservacion = false;
+        boolean reservacion = true;
         if (reservacion == true){
             if (Disponibilidad == true){
                 Disponibilidad = false;
